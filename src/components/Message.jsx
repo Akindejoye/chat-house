2ca61102edfdce4844ee4ad8f3f7e7968c7a833f
Akindejoye <tydruhill@gmail.com>
2023-04-09 22:56:50 +0100

    Chat Component

---
+++ src/components/Message.jsx
@@ -0,0 +1,22 @@
+const Message = () => {
+  return (
+    <div className="message owner">
+      <div className="messageInfo">
+        <img
+          src="https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=8ssXDNTp1XAPan8Bg6mJRwG7EXHshFO5o0v9SIj96nY="
+          alt="Chat Partner"
+        />
+        <span>just now</span>
+      </div>
+      <div className="messageContent">
+        <p>hello</p>
+        <img
+          src="https://media.istockphoto.com/id/1386479313/photo/happy-millennial-afro-american-business-woman-posing-isolated-on-white.jpg?s=612x612&w=0&k=20&c=8ssXDNTp1XAPan8Bg6mJRwG7EXHshFO5o0v9SIj96nY="
+          alt="Content"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Message;
