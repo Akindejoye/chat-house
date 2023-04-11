76824a564b4fe5ef5bc7360f8387cb6f17480db4
Akindejoye <tydruhill@gmail.com>
2023-04-11 15:48:09 +0100

    Registration Auth

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,47 +1,85 @@
 import { useState } from "react";
 import Add from "../img/addAvatar.png";
-import { createUserWithEmailAndPassword } from "firebase/auth";
-import { auth } from "../firebase";
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import { auth, storage, db } from "../firebase";
+import { doc, setDoc } from "firebase/firestore";
 
 const Register = () => {
-  const handleSubmit = (e) => {
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     const displayName = e.target[0].value;
     const email = e.target[1].value;
     const password = e.target[2].value;
-    // const file = e.target[3].file[0];
+    const file = e.target[3].files[0];
 
-    createUserWithEmailAndPassword(auth, email, password)
-      .then((userCredential) => {
-        //Signed In
-        const user = userCredential.user;
-        console.log(user);
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-      });
+    try {
+      const res = await createUserWithEmailAndPassword(auth, email, password);
+
+      console.log(res);
+      setError(false);
+
+      const storageRef = ref(storage, displayName);
+
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
+      // Register three observers:
+      uploadTask.on(
+        "state_changed",
+
+        (error) => {
+          setError(true);
+        },
+        () => {
+          getDownloadURL(uploadTask.snapshot.ref).then(async (downloadURL) => {
+            await updateProfile(res.user, {
+              displayName,
+              photoURL: downloadURL,
+            });
+            await setDoc(doc(db, "users", res.user.uid), {
+              uid: res.user.uid,
+              displayName,
+              email,
+              photoURL: downloadURL,
+            });
+          });
+        }
+      );
+    } catch (error) {
+      setError(true);
+    }
   };
 
   return (
-    <div className="formContainer">
-      <div className="formWrapper">
-        <span className="logo">Chat House</span>
-        <span className="title">Register</span>
-        <form onSubmit={handleSubmit}>
-          <input type="text" placeholder="Display Name" />
-          <input type="email" placeholder="Email" />
-          <input type="password" placeholder="Password" />
-          <input type="file" id="file" style={{ display: "none" }} />
-          <label htmlFor="file">
-            <img src={Add} alt="Label File" />
-            <span>Add an avatar</span>
-          </label>
-          <button>Sign Up</button>
-        </form>
-        <p>You do have an account? Login</p>
+    <>
+      <div className="formContainer">
+        <div className="formWrapper">
+          {error ? (
+            <div style={{ color: "red", paddingBottom: "7px" }}>
+              <h3>"Something went wrong"</h3>
+            </div>
+          ) : (
+            ""
+          )}
+          <span className="logo">Chat House</span>
+          <span className="title">Register</span>
+          <form onSubmit={handleSubmit}>
+            <input type="text" placeholder="Display Name" />
+            <input type="email" placeholder="Email" />
+            <input type="password" placeholder="Password" />
+            <input type="file" id="file" style={{ display: "none" }} />
+            <label htmlFor="file">
+              <img src={Add} alt="Label File" />
+              <span>Add an avatar</span>
+            </label>
+            <button>Sign Up</button>
+          </form>
+          <p>You do have an account? Login</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
